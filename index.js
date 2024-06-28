1337a255ba24befbfff48871d7eb55eeed914217
aaryan-sood <soodaaryan7@gmail.com>
2024-06-29 04:28:14 +0530

    added update route in crud functionality

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const mongoose=require('mongoose')
+const methodOverride=require('method-override')
 const path=require('path')
 const Campground=require('./models/campgrounds.js')
 
-app =express()
+const app =express()
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 .then(() => console.log('database connected'))
@@ -14,6 +15,7 @@ app.set('views',path.join(__dirname,'views'))
 
 // express middleware to get information from req.body
 app.use(express.urlencoded({extended : true}))
+app.use(methodOverride('_method'))      //express middleware to for methodOverride
 
 
 app.get('/',(req,res) => {
@@ -43,6 +45,19 @@ app.get('/campgrounds/:id',async(req,res) => {
     res.render('campgrounds/show.ejs',{campground})
 })
 
+app.get('/campgrounds/:id/edit',async(req,res) => {
+    let {id}=req.params
+    let campground = await Campground.findById(id);
+    res.render('campgrounds/edit.ejs',{campground})
+})
+
+app.patch('/campgrounds/:id',async(req,res) => {
+    let {id}=req.params
+    let campground=req.body.campground
+    let newCampground=await Campground.findByIdAndUpdate(id,campground,{runValidators : true,new : true})
+    console.log(newCampground)
+    res.redirect(`/campgrounds/${newCampground._id}`)
+})
 
 let port=3000
 app.listen(port,()=> {
