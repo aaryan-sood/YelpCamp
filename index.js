cbbc118ed51618fa5a8dcc7590be7abf930911f8
aaryan-sood <soodaaryan7@gmail.com>
2024-06-29 03:50:36 +0530

    added show,new and create route

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 app.set('views engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
+// express middleware to get information from req.body
+app.use(express.urlencoded({extended : true}))
+
 
 app.get('/',(req,res) => {
     // res.send('Hello from YelpCamp')
@@ -22,6 +25,24 @@ app.get('/campgrounds',async (req,res) => {
     const campgrounds=await Campground.find({})
     res.render('campgrounds/index.ejs',{campgrounds})
 })
+
+app.post('/campgrounds',async(req,res) => {
+   const campground=new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+    // new Campground()
+})
+
+app.get('/campgrounds/new',(req,res) => {
+    res.render('campgrounds/new.ejs')
+})
+
+app.get('/campgrounds/:id',async(req,res) => {
+    let {id}=req.params
+    let campground=await Campground.findById(id)
+    res.render('campgrounds/show.ejs',{campground})
+})
+
 
 let port=3000
 app.listen(port,()=> {
