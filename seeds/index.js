42211a2205777fe27c91768e6667dfb6439e488f
aaryan-sood <soodaaryan7@gmail.com>
2024-07-02 01:24:59 +0530

    Changed the model and added css to the index page

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDb=async ()=> {
     await Campground.deleteMany({})
     for(let i=0;i<50;i++){
         let random1000=Math.floor(Math.random() * 1000)
+        let price = Math.floor(Math.random() * 20) + 1
         const camp=new Campground({
             location : `${cities[random1000].city} , ${cities[random1000].state}`,
-            title : `${sample(data.descriptors)} ${sample(data.places)} `
+            title : `${sample(data.descriptors)} ${sample(data.places)} `,
+            image :  `https://picsum.photos/400?random=${Math.random()}`,
+            description : `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sit quam sapiente nostrum earum? Id facilis quod enim quam dignissimos nulla, laboriosam laborum nam ab ipsum a laudantium eos deleniti?`,
+            price : price
         })
         await camp.save()
     }
